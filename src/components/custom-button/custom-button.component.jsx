a6b11ae906b200721de590410ba66f2204560414
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-03-15 16:28:07 +0200

    add sign-in-and-sign-up page, sign-in, form-input and custom-button components

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./custom-button.styles.scss";
+
+const CustomButton = ({ children, ...restProps }) => {
+  return (
+    <button className="custom-button" {...restProps}>
+      {children}
+    </button>
+  );
+};
+
+export default CustomButton;
---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...restProps }) => {
+  return (
+    <div className="group">
+      <input className="form-input" onChange={handleChange} {...restProps} />
+      {/*{label ? (*/}
+      {/*  <label*/}
+      {/*    className={`${*/}
+      {/*      restProps.value.length ? "shrink" : ""*/}
+      {/*    } form-input-label`}*/}
+      {/*  >*/}
+      {/*    {label}*/}
+      {/*  </label>*/}
+      {/*) : null}*/}
+      {label && (
+        <label
+          className={`${
+            restProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
