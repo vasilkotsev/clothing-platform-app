badb3b020bd80f8caae08fab4aa595cb27d4cd92
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-05-29 18:50:36 +0300

    implements NotFoundPage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import NotFoundPage from "./pages/not-found-page/not-found-page.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import "./App.css";
 
@@ -45,7 +46,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/sign-in" component={SignInAndSignUpPage} />
+          <Route exact path="/sign-in" component={SignInAndSignUpPage} />
+          <Route path="/not-found" component={NotFoundPage} />
+          <Redirect to="/not-found" />
         </Switch>
       </React.Fragment>
     );
